169492372da9132c32f7f40d90388f4a88adb2cd
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-03-03 16:43:35 -0500

    Created portfolio.js
    This is the portfolio page where the stock info is pulled from IEXCloud. I used the iexcloud_api_wrapper module to pull the stock info. Then I draw its information out into an object for storing the symbols and pricing while an array stores the list that will be populated into the webpage. First you can see the object creation.

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+/* 
+This is the portfolio page where the stock info is pulled from IEXCloud. I used the iexcloud_api_wrapper module to pull the stock info.
+Then I draw its information out into an object for storing the symbols and pricing while an array stores the list that will be populated into the webpage.
+First you can see the object creation.
+*/
+
+let object = {};
+let array = [];
