01a4ada5e44fe66dc1905c08c87fa08dfc280bfc
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-03-03 16:29:36 -0500

    Created Index.js
    This is the first page of the web-based stock portfolio app.
    The design was supposed to feature functions ready to call a database to check if the user's email and password were found in the user table.
    However, I was not able to setup the database properly in time.
    The code starts by calling the http module in order to create a web server instance for the code to run.

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+This is the first page of the web-based stock portfolio app. 
+The design was supposed to feature functions ready to call a database to check if the user's email and password were found in the user table. 
+However, I was not able to setup the database properly in time.
+The code starts by calling the http module in order to create a web server instance for the code to run.
+ */
+let http=require('http')
